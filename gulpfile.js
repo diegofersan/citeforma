f5b85774a86a40095baf47bfd0596e977c51af71
Diego Fersan <diegofersan@gmail.com>
2020-12-20 23:54:35 -0300

    Commit inicial

--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,8 @@ const files = {
 function scssTask(){    
     return src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass()) // compile SCSS to CSS
-        .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
+        .pipe(sass({outputStyle: 'nested'})) // compile SCSS to CSS
+        .pipe(postcss([ autoprefixer() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
     ); // put final CSS in dist folder
